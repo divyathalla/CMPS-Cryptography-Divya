5ac206638729512e489befc1a5f7a7001c9a2b9d
Divya Thalla <tdivya.ks79@gmail.com>
2015-08-04 11:07:51 -0500

    Update ellipse.py

--- Divya.Thalla.Elliptical/ellipse.py
+++ Divya.Thalla.Elliptical/ellipse.py
@@ -46,8 +46,7 @@ def plotcurve(w,h,a,b,m,x1,y1,x2,y2,x3,y3):
 # Use a contour plot to draw the line (in pink) connecting our point.
     plt.contour(x.ravel(), y.ravel(), (y-y1)-m*(x-x1), [0],colors=('pink'))
 
-# I hard coded the third point, YOU will use good ol mathematics to find
-# the third point
+
     plt.plot(x3, y3,'yo')
 
 # Annotate point 3
